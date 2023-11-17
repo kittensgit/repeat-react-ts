f5d9d113436f2b742e1998fd833b9bf89fcffb66
nika <nikaonime@gmail.com>
2023-11-17 11:58:43 +0200

    props typing, interface, React.ReactNode(children)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Card from './components/Card';
 
 function App() {
-    return <div className="App">Hi, React!</div>;
+    return (
+        <div className="App">
+            <Card width="200px" height="90px">
+                <button>Click me</button>
+                <div>fdgfdg</div>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
