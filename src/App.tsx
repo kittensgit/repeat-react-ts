ae1cb5074f3f8c761cbbde651ca8be33831dd9c4
nika <nikaonime@gmail.com>
2023-11-18 14:29:27 +0200

    axios, get request(jsonplaceholder)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
+import { IUser } from './types/types';
+import axios from 'axios';
 
 const App: React.FC = () => {
-    const users = [
-        {
-            id: 1,
-            name: 'kirill',
-            email: 'kotl@gmail.com',
-            address: {
-                city: 'Novik',
-                street: 'ejak',
-                zipcode: 'hz',
-            },
-        },
-    ];
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        fetchUsers();
+    });
+
+    const fetchUsers = async () => {
+        try {
+            const response = await axios.get<IUser[]>(
+                'https://jsonplaceholder.typicode.com/users'
+            );
+            setUsers(response.data);
+        } catch (error) {
+            alert(error);
+        }
+    };
     return (
         <div className="App">
             <Card width="200px" height="200px" variant={CardVariant.outlined}>
